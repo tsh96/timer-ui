6e74453d0a2116f17ed2b38ad391d478b208e75e
Tan Sia How <tansiahow1996@gmail.com>
2023-09-09 18:09:29 +0800

    update VitePWA setting

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,33 @@ export default defineConfig({
     },
   },
   plugins: [
-    VitePWA(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'],
+      manifest: {
+        name: 'Timer',
+        short_name: 'Timer',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
     VueMacros({
       defineOptions: false,
       defineModels: false,
